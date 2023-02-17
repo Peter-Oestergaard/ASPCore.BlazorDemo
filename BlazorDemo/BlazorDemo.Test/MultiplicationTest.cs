6064c458e3930cc2e49dfbb6be66f6c9ad6e4f42
Peter Østergaard <peter@engelund.eu>
2023-02-17 19:39:28 +0100

    Multiplication tests

---
+++ BlazorDemo/BlazorDemo.Test/MultiplicationTest.cs
@@ -0,0 +1,159 @@
+namespace BlazorDemo.Test;
+
+using System.Globalization;
+using AngleSharp.Dom;
+using Pages;
+using Bunit;
+
+public sealed class MultiplicationTest : TestContext
+{
+    private readonly IElement _inputNum1;
+    private readonly IElement _inputNum2;
+    private readonly IElement _addButton;
+    private readonly IElement _result;
+
+    public MultiplicationTest()
+    {
+        // Arrange
+
+        // Dealing with decimal separators we need to lock the test to a specific culture
+        // in order to have stable string conversions of doubles
+        CultureInfo.CurrentCulture = new CultureInfo("da-DK");
+
+        IRenderedComponent<Calculator> calculatorComponent = RenderComponent<Calculator>();
+
+        _inputNum1 = calculatorComponent.Find("input[placeholder='Enter First Number']");
+        _inputNum2 = calculatorComponent.Find("input[placeholder='Enter Second Number']");
+        _addButton = calculatorComponent.Find("button:contains('Multiply (X)')");
+        _result = calculatorComponent.Find("input[readonly]");
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    [InlineData(1, -1)]
+    public void MultiplyingTwoIntegers_GivesCorrectResult(int a, int b)
+    {
+        // Act
+        _inputNum1.Change(a.ToString());
+        _inputNum2.Change(b.ToString());
+        _addButton.Click();
+
+        // Assert
+        Assert.Equal((a * b).ToString(), _result.GetAttribute("value"));
+    }
+
+    [Theory]
+    [InlineData("0", "-1")]
+    [InlineData("-0", "-1")]
+    [InlineData("-0", "-0")]
+    public void MultiplyingSignedNumbersAndSignedZero_GivesCorrectResult(string a, string b)
+    {
+        // "-0" can be entered into either box so we need to test for it.
+
+        // Act
+        _inputNum1.Change(a);
+        _inputNum2.Change(b);
+        _addButton.Click();
+
+        // Assert
+        Assert.True(double.TryParse(a, out double ad));
+        Assert.True(double.TryParse(b, out double bd));
+        Assert.True(double.TryParse(_result.GetAttribute("value"), out double resultAsDouble));
+        
+        // Because of some magic and IEEE 754 some values results in negative zero. Negative zero and zero should be
+        // indistinguishable from each other so we Math.Abs() it away to have our test succeed.
+        double expectedDouble = ad * bd;
+        string expected = Math.Abs(expectedDouble).ToString(CultureInfo.CurrentCulture);
+        string actual = Math.Abs(resultAsDouble).ToString(CultureInfo.CurrentCulture);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0.0)]
+    [InlineData(0, 0.1)]
+    [InlineData(1, 0.1)]
+    [InlineData(-1, -1.0)]
+    public void MultiplyingIntegersAndDoubles_GivesCorrectResult(int a, double b)
+    {
+        // Arrange
+        string expected = (a * b).ToString(CultureInfo.CurrentCulture);
+
+        // Act
+        _inputNum1.Change(a.ToString());
+        _inputNum2.Change(b.ToString(CultureInfo.CurrentCulture));
+        _addButton.Click();
+
+        // Assert
+        string result = _result.GetAttribute("value");
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(0.0, 0.0)]
+    [InlineData(0.0, 0.1)]
+    [InlineData(1.0, 0.1)]
+    [InlineData(-1.0, -1.0)]
+    [InlineData(2.0, 3.0)]
+    public void MultiplyingDoublesAndDoubles_GivesCorrectResult(double a, double b)
+    {
+        // Arrange
+        string expected = (a * b).ToString(CultureInfo.CurrentCulture);
+
+        // Act
+        _inputNum1.Change(a.ToString(CultureInfo.CurrentCulture));
+        _inputNum2.Change(b.ToString(CultureInfo.CurrentCulture));
+        _addButton.Click();
+
+        // Assert
+        string result = _result.GetAttribute("value");
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void MultiplyingDoubleMaxToDoubleMax_GivesPositiveInfinity()
+    {
+        double maxDouble = double.MaxValue;
+
+        // Act
+        _inputNum1.Change(maxDouble.ToString(CultureInfo.CurrentCulture));
+        _inputNum2.Change(maxDouble.ToString(CultureInfo.CurrentCulture));
+        _addButton.Click();
+
+        // Assert
+        double expected = double.PositiveInfinity;
+        Assert.Equal(expected.ToString(CultureInfo.CurrentCulture), _result.GetAttribute("value"));
+    }
+    
+    [Fact]
+    public void MultiplyingNegativeDoubleMaxToNegativeDoubleMax_GivesPositiveInfinity()
+    {
+        double negativeMaxDouble = double.MaxValue * -1;
+
+        // Act
+        _inputNum1.Change(negativeMaxDouble.ToString(CultureInfo.CurrentCulture));
+        _inputNum2.Change(negativeMaxDouble.ToString(CultureInfo.CurrentCulture));
+        _addButton.Click();
+
+        // Assert
+        double expected = double.PositiveInfinity;
+        Assert.Equal(expected.ToString(CultureInfo.CurrentCulture), _result.GetAttribute("value"));
+    }
+    
+    [Fact]
+    public void MultiplyingDoubleMaxToNegativeDoubleMax_GivesNegativeInfinity()
+    {
+        double maxDouble = double.MaxValue;
+        double negativeMaxDouble = double.MaxValue * -1;
+
+        // Act
+        _inputNum1.Change(maxDouble.ToString(CultureInfo.CurrentCulture));
+        _inputNum2.Change(negativeMaxDouble.ToString(CultureInfo.CurrentCulture));
+        _addButton.Click();
+
+        // Assert
+        double expected = double.NegativeInfinity;
+        Assert.Equal(expected.ToString(CultureInfo.CurrentCulture), _result.GetAttribute("value"));
+    }
+}
